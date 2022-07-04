010d30d5aa8ddceeeedd3d04a3a6425bb93707f3
isapiza <isaiasbritez52@gmail.com>
2022-07-04 16:49:01 -0300

    ejercicio 1

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,17 +1,10 @@
 /*
+Isaias Britez
+DIV X
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-var a = prompt ("ingrese un numero"); var b = prompt ("ingrese un numero"); a = parseInt (a);
-b = parseInt (b);
-var resultado = a + b;
-alert(resultado);
-
-
-
-
-
-	
+ alert("esto funciona de maravila");
 }
 
