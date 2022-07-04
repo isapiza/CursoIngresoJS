6d3dc342e091ef0ded868598a941419f7012310d
isapiza <isaiasbritez52@gmail.com>
2022-07-04 18:14:42 -0300

    nuevo

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,11 @@
 /*
+Isaias Britez
+DIV: X
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	let nombre = prompt("Escribe tu nombre");
+	alert("Tu nombre es: " + nombre); 
 }
 
