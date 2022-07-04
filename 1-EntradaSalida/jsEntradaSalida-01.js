934b4ae26f49b32590527ac8615b43f50233383a
isapiza <isaiasbritez52@gmail.com>
2022-07-04 16:08:06 -0300

    lol

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,15 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hello man");
+var a = prompt ("ingrese un numero"); var b = prompt ("ingrese un numero"); a = parseInt (a);
+b = parseInt (b);
+var resultado = a + b;
+alert(resultado);
+
+
+
+
+
+	
 }
 
